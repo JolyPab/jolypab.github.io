ec3e49beeeee9deac6b340f663e37c416a3120ff
JolyPab <pablik.pico@gmail.com>
2024-08-10 01:01:44 +0300

    ddd
    aasas

---
+++ php/main.php
@@ -0,0 +1,40 @@
+<!-- main.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Страница-визитка</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Моя Страница Визитка</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Контакты</a></li>
+        </ul>
+    </nav>
+    <main class="container">
+        <section class="profile">
+            <h2>Обо мне</h2>
+            <img src="https://avatars.akamai.steamstatic.com/7e06efdf176687297000359c3190b5ca451580fb_full.jpg" alt="Мое фото" class="profile-img">
+            <p>Привет! Меня зовут Пабло Пико, я будующий Пентестер из питера. Мне 19 лет, и я люблю кс и пентест.</p>
+        </section>
+        <section class="skills">
+            <h2>Мои навыки</h2>
+            <ul>
+                <li>HTML/CSS</li>
+                <li>JavaScript</li>
+                <li>PHP</li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>© 2024 Мой сайт. Все права защищены.</p>
+    </footer>
+</body>
+</html>
